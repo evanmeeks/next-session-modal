384d0df0605b8044f71c1eaad1a4b10830db3213
Evan Meeks <evan.meeks@gmail.com>
2023-04-04 01:27:50 -0500

    next setup

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,6 +1,6 @@
 import "next-auth/jwt"
 
-// Read more at: https://next-auth.js.org/getting-started/typescript#module-augmentation
+//  Read more at: https://next-auth.js.org/getting-started/typescript#module-augmentation
 
 declare module "next-auth/jwt" {
   interface JWT {
